3d7104d5fa75ad671052a9790b8e52e057b25d4f
kev <kevinyu211@yahoo.com>
2020-06-01 22:24:14 +0800

    done

--- yolov3_onnx/set_cuda.sh
+++ yolov3_onnx/set_cuda.sh
@@ -7,6 +7,6 @@ if ! grep 'cuda/bin' ${HOME}/.bashrc > /dev/null ; then
   echo >> ${HOME}/.bashrc
   echo "export PATH=/usr/local/cuda/bin:\${PATH}" >> ${HOME}/.bashrc
   echo "export LD_LIBRARY_PATH=/usr/local/cuda/lib64:\${LD_LIBRARY_PATH}" >> ${HOME}/.bashrc
-  export PATH=$PATH:/usr/local/cuda/bin >> ${HOME}/.bashrc
+  echo "export PATH=$PATH:/usr/local/cuda/bin" >> ${HOME}/.bashrc
   source ~/.bashrc
 fi
