74390a3930f2acd707688cc56fdaa1313cc41f78
kev <kevinyu211@yahoo.com>
2020-06-01 21:29:20 +0800

    done

--- yolov3_onnx/set_cuda.sh
+++ yolov3_onnx/set_cuda.sh
@@ -3,8 +3,9 @@
 set -e
 
 if ! grep 'cuda/bin' ${HOME}/.bashrc > /dev/null ; then
-  echo "** Add CUDA path into ~/.bashrc"
+  echo "[BASH]  Add CUDA path into ~/.bashrc"
   echo >> ${HOME}/.bashrc
   echo "export PATH=/usr/local/cuda/bin:\${PATH}" >> ${HOME}/.bashrc
   echo "export LD_LIBRARY_PATH=/usr/local/cuda/lib64:\${LD_LIBRARY_PATH}" >> ${HOME}/.bashrc
+  source ~/.bashrc
 fi
