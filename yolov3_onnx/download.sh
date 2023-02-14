f0ad00c562555a00afd4c517dcfcb905c4fd1544
Kevin Yu <kevinyu211@yahoo.com>
2023-02-14 20:44:21 +0800

    update artifact source

--- yolov3_onnx/download.sh
+++ yolov3_onnx/download.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Downloading YOLOv3 config and weights..."
-wget https://objectstorage.ca-toronto-1.oraclecloud.com/n/yzpqsgba6ssd/b/bucket-20200425-1646-weights/o/yolov3.cfg -q --show-progress --no-clobber
+wget https://objectstorage.us-sanjose-1.oraclecloud.com/n/axdjylkbplt1/b/artifacts/o/yolov3.cfg -q --show-progress --no-clobber
 wget https://objectstorage.us-sanjose-1.oraclecloud.com/n/axdjylkbplt1/b/artifacts/o/yolov3.weights -q --show-progress --no-clobber
 
 echo
@@ -17,7 +17,7 @@ ln -sf yolov3.weights yolov3-608.weights
 
 echo
 echo "Downloading YOLOv3-tiny config and weights..."
-wget https://objectstorage.ca-toronto-1.oraclecloud.com/n/yzpqsgba6ssd/b/bucket-20200425-1646-weights/o/yolov3-tiny.cfg -q --show-progress --no-clobber
+wget https://objectstorage.us-sanjose-1.oraclecloud.com/n/axdjylkbplt1/b/artifacts/o/yolov3-tiny.cfg -q --show-progress --no-clobber
 wget https://objectstorage.us-sanjose-1.oraclecloud.com/n/axdjylkbplt1/b/artifacts/o/yolov3-tiny.weights -q --show-progress --no-clobber
 
 echo
